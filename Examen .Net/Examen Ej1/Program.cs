e6827b57e8e4eef751f9f0ed6e803b8af398abe2
MLG96 <54599039+MLG96@users.noreply.github.com>
2020-06-30 21:52:44 -0300

    Add files via upload

---
+++ Examen .Net/Examen Ej1/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace Examen_Ej1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int DigitoIngresado;
+
+            Console.WriteLine("Ingrese un numero:");
+
+            if (Int32.TryParse(Console.ReadLine(), out DigitoIngresado) !=true)
+            {
+                Console.WriteLine("No es un tipo de dato valido.");
+            }
+            else
+            {
+                GenerarRectangulo(DigitoIngresado);
+            }
+            Console.ReadKey();
+        }
+
+        public static void GenerarRectangulo(int DigitoIngresado)
+        {
+            for (int i = 0; i < 5; i++)
+            {  
+                Console.WriteLine();
+
+            for (int j = 0; j < 3; j++)
+                
+                if (i == 0 || i == 4 || j == 0 || j == 2 )
+                {
+                Console.Write($"{DigitoIngresado}");
+                }
+                else
+                {
+                Console.Write(" ");
+                }
+            }
+        }
+    }
+}
+
+
+/*
+Ingresa 4
+444
+4 4
+4 4
+4 4
+444 
+*/
---
+++ Examen .Net/Examen Ej2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Examen_Ej2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int NumIngresado;
+
+            Console.WriteLine("Ingrese la cantidad de tablas:");
+
+            if (Int32.TryParse(Console.ReadLine(), out NumIngresado) != true)
+            {
+                Console.WriteLine("No es un tipo de dato valido.");
+            }
+            else
+            {
+                Tablas(NumIngresado);
+            }
+            Console.ReadKey();
+        }
+
+        public static void Tablas(int numIngresado)
+        {
+            for (int i = 0; i < numIngresado; i++)
+            {
+
+                int Resultado = 0;
+                Console.WriteLine("Pulse una tecla:");
+                Console.ReadKey();
+
+                for (int j = 0; j < 10; j++)
+                {
+                    int Multiplicador = j + 1;
+                    Resultado = (i + 1) * Multiplicador;
+                    Console.WriteLine($"{i + 1}*{Multiplicador} = {Resultado}");
+                }
+            }
+        }
+    }
+}
